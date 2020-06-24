469a57d0c0e2f42474df6d29e7816f0fb94178b0
Mateus P Silva <mateus.p.silva@ufv.br>
2020-06-24 18:05:45 -0300

    vector sum flexible

--- vector_sum.cu
+++ vector_sum.cu
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <time.h>
+#include <errno.h>
+#include <limits.h> 
+#include <stdlib.h>
 #include "common.cuh"
 
-#define VECTOR_QNT 2000000
-#define VECTOR_SIZE VECTOR_QNT*sizeof(int)
-
-void initializeVector(int * vector)
+void initializeVector(int * vector, int VECTOR_QNT)
 {
     for(size_t i=0; i<VECTOR_QNT; i++)
     {
@@ -13,7 +13,7 @@ void initializeVector(int * vector)
     }
 }
 
-void CPUVectorSum(int * a, int * b, int * c)
+void CPUVectorSum(int * a, int * b, int * c, int VECTOR_QNT)
 {
     for(size_t i=0; i<VECTOR_QNT; i++)
     {
@@ -21,7 +21,7 @@ void CPUVectorSum(int * a, int * b, int * c)
     }
 }
 
-__global__ void GPUVectorSum(int * a, int * b, int * c) {
+__global__ void GPUVectorSum(int * a, int * b, int * c, int VECTOR_QNT) {
     int n = VECTOR_QNT;
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     for (int i = idx; i < n; i += blockDim.x * gridDim.x)
@@ -30,7 +30,7 @@ __global__ void GPUVectorSum(int * a, int * b, int * c) {
     }
 }
 
-int checkCorrect(int * vector)
+int checkCorrect(int * vector, int VECTOR_QNT)
 {
     int errors = 0;
 
@@ -45,8 +45,33 @@ int checkCorrect(int * vector)
     return errors;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    if(argc!=2)
+    {
+        printf("Please input only the vectorsize!\n");
+        exit(1);
+    }
+
+    char *p;
+    int VECTOR_QNT;
+    int VECTOR_SIZE;
+
+    errno = 0;
+    long conv = strtol(argv[1], &p, 10);
+
+    // Check for errors: e.g., the string does not represent an integer
+    // or the integer is larger than int
+    if (errno != 0 || *p != '\0' || conv > INT_MAX) {
+        // Put here the handling of the error, like exiting the program with
+        // an error message
+    } else {
+        // No error
+        VECTOR_QNT = conv;    
+    }
+
+    VECTOR_SIZE = VECTOR_QNT*sizeof(int);
+
     int * a_cpu = (int*)malloc(VECTOR_SIZE);
     int * b_cpu = (int*)malloc(VECTOR_SIZE);
     int * c_cpu = (int*)malloc(VECTOR_SIZE);
@@ -58,8 +83,8 @@ int main()
     clock_t start, end;
     dim3 grid, block;
 
-    initializeVector(a_cpu);
-    initializeVector(b_cpu);
+    initializeVector(a_cpu, VECTOR_QNT);
+    initializeVector(b_cpu, VECTOR_QNT);
 
     // Allocating memory for GPU DMAs
     cudaMalloc((int **)&a_gpu, VECTOR_SIZE);
@@ -70,19 +95,19 @@ int main()
     cudaMemcpy(b_gpu, b_cpu, VECTOR_SIZE, cudaMemcpyHostToDevice);
 
     start = clock();
-    CPUVectorSum(a_cpu,b_cpu,c_cpu);
+    CPUVectorSum(a_cpu,b_cpu,c_cpu, VECTOR_QNT);
     end = clock();
-    printf("Time=%f, Errors on CPU = %d\n", ((double) (end - start)) / CLOCKS_PER_SEC, checkCorrect(c_cpu));
+    printf("Time=%f, Errors on CPU = %d\n", ((double) (end - start)) / CLOCKS_PER_SEC, checkCorrect(c_cpu, VECTOR_QNT));
 
     start = clock();
     GetGPUGridConfig(grid, block);
-    GPUVectorSum<<<grid,block>>>(a_gpu,b_gpu,c_gpu);
+    GPUVectorSum<<<grid,block>>>(a_gpu,b_gpu,c_gpu, VECTOR_QNT);
     CheckGpuPanic();
     cudaDeviceSynchronize();
     end = clock();
 
     cudaMemcpy(c_cpu, c_gpu, VECTOR_SIZE, cudaMemcpyDeviceToHost);
-    printf("Time=%f, Errors on GPU = %d\n", ((double) (end - start)) / CLOCKS_PER_SEC, checkCorrect(c_cpu));
+    printf("Time=%f, Errors on GPU = %d\n", ((double) (end - start)) / CLOCKS_PER_SEC, checkCorrect(c_cpu, VECTOR_QNT));
 
     free(a_cpu);
     free(b_cpu);
